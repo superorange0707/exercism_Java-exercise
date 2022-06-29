fbd4c1faae6c7c38377890bf391d939d9bda3def
superorange0707 <77090682+superorange0707@users.noreply.github.com>
2022-06-29 20:55:26 +0100

    Update ReverseString.java

--- reverse-string/src/main/java/ReverseString.java
+++ reverse-string/src/main/java/ReverseString.java
@@ -6,6 +6,10 @@ class ReverseString {
         for (int i = length -1; i>=0; i--){
             string1 =  string1 + try1[i];
         }
+        // for (int i=0; i<length; i++){
+        //     string1 = inputString.charAt(i) + string1;
+        // }
         return string1;
+        
     }
 }
