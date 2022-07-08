0d86845263b0f198d3a311f3a3a54608df79ad04
superorange0707 <77090682+superorange0707@users.noreply.github.com>
2022-07-08 13:00:03 +0100

    Update IsogramChecker.java

--- Exercism/Exercism_solution/String/isogram/src/main/java/IsogramChecker.java
+++ Exercism/Exercism_solution/String/isogram/src/main/java/IsogramChecker.java
@@ -1,23 +1,36 @@
+import java.util.*;
 class IsogramChecker {
 
     boolean isIsogram(String phrase) {
-        // make all letters in the string are lower
-        phrase = phrase.toLowerCase();
+        // make all letters in the string are lower 
         // remove all space and -
-        phrase = phrase.replaceAll(" ", "");
-        phrase = phrase.replaceAll("-", "");
+        phrase = phrase.toLowerCase().replaceAll(" ", "").replaceAll("-", "");
         int length = phrase.length();
 
         // two traversal to compare the character in the string
-        for (int i = 0; i <length; i++){
-            char cp = phrase.charAt(i);
-            for (int j= i+1; j<length; j++){
-                char ct = phrase.charAt(j);
-                if (cp == ct){
-                    return false;
-                }
-            }
+       
+        // for (int i = 0; i <length; i++){
+        //     char cp = phrase.charAt(i);
+        //     for (int j= i+1; j<length; j++){
+        //         char ct = phrase.charAt(j);
+        //             if (cp == ct){
+        //                 return false;
+        //             }
+        //         }
+        //     }
+        // return true;
+
+        
+        // use Set
+        LinkedHashSet<Character> set = new LinkedHashSet<>();
+        for(int i=0; i<length; i++){
+            set.add(phrase.charAt(i));
         }
-        return true;
+        if (set.size() == length){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
